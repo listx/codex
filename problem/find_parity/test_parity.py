0f6febdaa213cd31bf57d27085cad3a89f643a8a
Linus Arver <linusarver@gmail.com>
2023-04-20 03:43:40 -0700

    parity: add some more discussion

--- problem/find_parity/test_parity.py
+++ problem/find_parity/test_parity.py
@@ -14,6 +14,19 @@ def brute_force_2(word):
     word &= word - 1
   return parity_bit
 
+def brute_force_3(word):
+  word ^= word >> 32
+  word ^= word >> 16
+  word ^= word >> 8
+  word ^= word >> 4
+  word ^= word >> 2
+  word ^= word >> 1
+  return word & 1
+
+def caching(word):
+  # TODO
+  return
+
 class TestParity(unittest.TestCase):
   cases = [
     (0b0, 0),
@@ -28,6 +41,8 @@ class TestParity(unittest.TestCase):
     for word, parity_bit in self.cases:
       self.assertEqual(brute_force_1(word), parity_bit)
       self.assertEqual(brute_force_2(word), parity_bit)
+      self.assertEqual(brute_force_3(word), parity_bit)
+      self.assertEqual(caching(word), parity_bit)
 
 if __name__ == "__main__":
   unittest.main(exit=False)
