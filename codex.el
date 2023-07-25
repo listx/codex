5ac6fa42e8b73654ea1379042c5bf5a825c59a38
Linus Arver <linusarver@gmail.com>
2023-07-24 23:02:28 -0700

    customize title text

--- codex.el
+++ codex.el
@@ -6,3 +6,13 @@
 
 (setq org-cite-csl-styles-dir
       (concat (getenv "LILAC_ROOT") "/deps/styles/"))
+
+; See https://stackoverflow.com/a/27285582/437583.
+(setq lilac-html-head
+      (concat
+       "<link rel=\"stylesheet\" type=\"text/css\" href=\"codex.css\" />\n"
+       "<link rel=\"stylesheet\" href="
+        "\"https://fonts.googleapis.com/css2"
+        "?family=Bungee+Shade:wght@400"
+       "\">"
+       ))
