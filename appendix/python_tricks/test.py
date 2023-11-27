a55b0f9e33d95318327a63e999c61378a982aa94
Linus Arver <linus@ucla.edu>
2023-11-27 00:30:42 -0800

    appendix: add example for customized sorting

--- appendix/python_tricks/test.py
+++ appendix/python_tricks/test.py
@@ -4,7 +4,10 @@ def is_palindrome(s: str) -> bool:
   return all(s[i] == s[~i] for i in range(len(s) // 2))
 
 class Test(unittest.TestCase):
-
+  def test_sort(self):
+      xs = [("a", 9), ("b", 5), ("c", 1)]
+      result = sorted(xs, key=lambda x: x[1])
+      self.assertEqual(result, [("c", 1), ("b", 5), ("a", 9)])
   def test_indices(self):
     self.assertEqual('abcdefg'[0], 'a')
     self.assertEqual('abcdefg'[1], 'b')
@@ -22,9 +25,6 @@ class Test(unittest.TestCase):
     self.assertEqual('abcdefg'[2:], 'cdefg')
     self.assertEqual('abcdefg'[1000:], '')
     self.assertEqual('abcdefg'[:1000], 'abcdefg')
-
-  def test_simple_cases(self):
-    pass
 
 if __name__ == "__main__":
   unittest.main(exit=False)
